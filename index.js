6949b66e019f7f52710f70e347f6ff3344a7bf49
Amrutha67 <ammua7909@gmail.com>
2021-04-03 16:17:25 +0530

    nodejs

---
+++ index.js
@@ -0,0 +1,10 @@
+var a=59
+if 
+(a%2==0)
+{
+    console.log("Even number")
+}
+else
+{
+    console.log("Odd number")
+}
---
+++ index2.js
@@ -0,0 +1,8 @@
+var a=88
+if(a%8==0)
+{
+    console.log("Number is divisible by 8")
+}
+else {
+    console.log("Number is not divisible by 8")
+}
